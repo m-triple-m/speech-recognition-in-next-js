5d92a0d4e75b9d0c40892d487570a2d8a6f57f7b
m-triple-m <triplem656@gmail.com>
2024-04-01 11:35:45 +0530

    added speech recognition

---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+'use client';
+// import 'regenerator-runtime/runtime';
+import React from 'react';
+
+const Home = () => {
+
+  return (
+    <div>
+      {/* <button className='text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg'>Nice</button> */}
+      <h1 className='bg-blue'>Home</h1>
+    </div>
+  )
+}
+
+export default Home;
